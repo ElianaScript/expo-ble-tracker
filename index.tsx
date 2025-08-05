c8cb23c88b0b8ee24b0555761d61b5deca759904
ElianaScipt <eliana.liantonio@gmail.com>
2025-08-05 11:23:43 -0400

    updates for testing

--- index.tsx
+++ index.tsx
@@ -1,1 +1,9 @@
-import 'expo-router/entry';
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+const App = () => {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+};
+
+registerRootComponent(App);
